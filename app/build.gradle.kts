330a1179c8fc0236fd6d76cc4a3903b87eeaf035
kleberson154 <kleberson55@hotmail.com>
2025-07-03 15:32:31 -0300

    Adicionando as activities ao manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.kleberson.appmedical"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kleberson.appmedical"
