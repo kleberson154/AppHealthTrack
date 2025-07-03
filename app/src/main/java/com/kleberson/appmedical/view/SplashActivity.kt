3e82d871a2df75fc155996ac6ea82a1368dc521b
kleberson154 <kleberson55@hotmail.com>
2025-07-03 13:17:21 -0300

    Projeto iniciado com tela splash

---
+++ app/src/main/java/com/kleberson/appmedical/view/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.kleberson.appmedical.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.kleberson.appmedical.MainActivity
+import com.kleberson.appmedical.R
+
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
